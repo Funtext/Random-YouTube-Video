36bf6ce5ca443873b2bbd633b70f3c2a1bb4cf41
Marco Ceccon <m.ceccon05@gmail.com>
2020-02-20 18:44:52 +0100

    Update app.py

--- app.py
+++ app.py
@@ -14,8 +14,6 @@ postfix = [' MOV', '.MOV', ' .MOV']
 def _index():
   youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey=DEVELOPER_KEY)
   search_response = youtube.search().list(q=random.choice(prefix) + str(random.randint(999, 9999)) + random.choice(postfix),part='snippet',maxResults=5).execute()
-  videos = []
   for search_result in search_response.get('items', []):
     if search_result['id']['kind'] == 'youtube#video':
-      videos.append('%s' % (search_result['id']['videoId']))
-      return Response(videos[random.randint(0, 2)])
+      return Response(search_result['id']['videoId'])
