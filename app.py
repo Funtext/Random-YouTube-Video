1bb149ee2d98c6612f27b7301397121e568d4d06
Marco Ceccon <m.ceccon05@gmail.com>
2020-02-20 18:03:35 +0100

    Update app.py

--- app.py
+++ app.py
@@ -8,18 +8,14 @@ YOUTUBE_API_SERVICE_NAME = 'youtube'
 YOUTUBE_API_VERSION = 'v3'
 prefix = ['IMG ', 'IMG_', 'IMG-', 'DSC ']
 postfix = [' MOV', '.MOV', ' .MOV']
-def youtube_search():
-youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey=DEVELOPER_KEY)
-search_response = youtube.search().list(
-q=random.choice(prefix) + str(random.randint(999, 9999)) + random.choice(postfix),part='snippet',maxResults=5).execute()
-videos = []
-for search_result in search_response.get('items', []):
-if search_result['id']['kind'] == 'youtube#video':
-videos.append('%s' % (search_result['id']['videoId']))
-return (videos[random.randint(0, 2)])
+
 @app.route('/')
 @cross_origin()
 def _index():
-return Response(json.dumps(youtube_search()), mimetype='application/json')
-if __name__ == '__main__':
-app.run(debug=False, use_reloader=True)
+  youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey=DEVELOPER_KEY)
+  search_response = youtube.search().list(q=random.choice(prefix) + str(random.randint(999, 9999)) + random.choice(postfix),part='snippet',maxResults=5).execute()
+  videos = []
+  for search_result in search_response.get('items', []):
+    if search_result['id']['kind'] == 'youtube#video':
+      videos.append('%s' % (search_result['id']['videoId']))
+      return Response(json.dumps(videos[random.randint(0, 2)]), mimetype='application/json')
